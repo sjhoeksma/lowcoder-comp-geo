4c9c8e90c585fede106e4af8e6db363de09958d9
Sierk Hoeksma <sierk@hoeksma.org>
2024-03-28 20:43:26 +0100

    fix: moved getFormat to source

--- src/vendors/helpers/Layers.js
+++ src/vendors/helpers/Layers.js
@@ -272,7 +272,7 @@ export function setFeatures(map, data, name, clear) {
       source.clear()
       undos.forEach((c) => { c.clear() })
     }
-    const reader = (layer.getFormat ? layer.getFormat() : null) || new GeoJSON()
+    const reader = (source.getFormat ? source.getFormat() : null) || new GeoJSON()
     if (reader && data) {
       //Now add the features based on types
       if (Array.isArray(data)) {
