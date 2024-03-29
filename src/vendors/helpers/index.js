93ef942c9808ca8b281d9476e3589db58363d419
Sierk Hoeksma <sierk@hoeksma.org>
2024-03-29 10:03:33 +0100

    doc: index

--- src/vendors/helpers/index.js
+++ src/vendors/helpers/index.js
@@ -1,6 +1,16 @@
-//Base exports
+
+/**
+ * Exports helper functions and components related to:
+ * - Layers
+ * - Popups 
+ * - Styles
+ * - Animations
+ * - Screen resize detection
+ * - DeepMerge
+ */
 export * from "./Layers";
 export * from "./Popup";
 export * from "./Styles";
 export * from "./Animate";
 export * from "./useScreenSize";
+export * from "./DeepMerge";
