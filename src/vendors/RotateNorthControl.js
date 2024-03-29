398a13246eaeb118c71fca32377c383cb11ee77a
Sierk Hoeksma <sierk@hoeksma.org>
2024-03-29 10:13:40 +0100

    doc: RotateNorthControls

--- src/vendors/RotateNorthControl.js
+++ src/vendors/RotateNorthControl.js
@@ -1,26 +1,27 @@
 import { Control } from 'ol/control';
 
 /**
- * Creates a new control to rotate the map to north.
+ * Creates a new control with a button to rotate the map to north. 
+ * Extends the OpenLayers Control class.
  */
 export default class RotateNorthControl extends Control {
-    constructor(options = {}) {
-      const button = document.createElement('button');
-      button.innerHTML = 'N';
-  
-      const element = document.createElement('div');
-      element.className = 'rotate-north ol-unselectable ol-control';
-      element.appendChild(button);
-  
-      super({
-        element: element,
-        target: options.target,
-      });
-  
-      button.addEventListener('click', this.handleRotateNorth.bind(this), false);
-    }
-  
-    handleRotateNorth() {
-      this.getMap().getView().setRotation(0);
-    }
+  constructor(options = {}) {
+    const button = document.createElement('button');
+    button.innerHTML = 'N';
+
+    const element = document.createElement('div');
+    element.className = 'rotate-north ol-unselectable ol-control';
+    element.appendChild(button);
+
+    super({
+      element: element,
+      target: options.target,
+    });
+
+    button.addEventListener('click', this.handleRotateNorth.bind(this), false);
   }
+
+  handleRotateNorth() {
+    this.getMap().getView().setRotation(0);
+  }
+}
