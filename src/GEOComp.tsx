75e47036ff4d48f794c3e4fb4047a4b1da16fdfb
Sierk Hoeksma <sierk@hoeksma.org>
2024-07-20 08:02:02 +0200

    fix: newHeight

--- src/GEOComp.tsx
+++ src/GEOComp.tsx
@@ -285,10 +285,13 @@ var GEOComp = (function () {
               //TODO: Take care of margin and padding, but also that of parents
               var newHeight = eventObj.windowSize.height - eventObj.bounds.top - offset * 2 - (props.resizeOffset || 0)
               eventObj.newHeight = newHeight
-              //eventObj.element.style.height = `${newHeight}px`
-              setDimensions({ width: dimensions.width, height: newHeight })
-              if (featureEnabled("debug"))
-                console.debug("Resized done", newHeight)
+              if (newHeight != dimensions.height) {
+                eventObj.heightChanged = true
+                //eventObj.element.style.height = `${newHeight}px`
+                setDimensions({ width: dimensions.width, height: newHeight })
+                if (featureEnabled("debug"))
+                  console.debug("Resized done", newHeight)
+              }
             }
             break
           default:
